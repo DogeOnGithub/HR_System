7fc07422b293f24eb4698f2dc9fc7587d0e62138
TjSanshao <1305705188@qq.com>
2017-12-03 00:44:00 +0800

    加了档案管理的控制器和档案登记的视图，断网了，写不了

---
+++ HR_System/Controllers/StaffManageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HR_System.Filters;
+
+namespace HR_System.Controllers
+{
+    [LoginUserAuthorization]
+    public class StaffManageController : Controller
+    {
+        /// <summary>
+        /// 处理档案登记请求
+        /// </summary>
+        /// <returns>返回档案登记视图</returns>
+        public ActionResult StaffRegist()
+        {
+            return View();
+        }
+    }
+}
