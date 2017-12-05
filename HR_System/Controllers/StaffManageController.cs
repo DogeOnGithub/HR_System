a0dee865b30ebe51c99477da01973ccd7031e8b0
TjSanshao <1305705188@qq.com>
2017-12-06 02:34:15 +0800

    基本完成档案登记页面，完善修改薪酬项目数值时的oninput事件，即现在修改项目金额的时候总金额也会跟着变化

--- HR_System/Controllers/StaffManageController.cs
+++ HR_System/Controllers/StaffManageController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HR_System.Filters;
+using HR_SystemIDAL;
+using HR_SystemIBLL;
+using HR_SystemBLL;
+using Model;
 
 namespace HR_System.Controllers
 {
@@ -16,6 +20,42 @@ namespace HR_System.Controllers
         /// <returns>返回档案登记视图</returns>
         public ActionResult StaffRegist()
         {
+
+            IOrgBLL orgBLL = new OrgBLL();
+
+            IOccupationBLL occupationBLL = new OccupationBLL();
+
+            //装载1级机构
+            List<FirstOrg> fList = orgBLL.GetAllFirstOrg();
+            List<Models.FirstOrg> firstOrgList = new List<Models.FirstOrg>();
+            foreach (var fo in fList)
+            {
+                Models.FirstOrg tempFo = new Models.FirstOrg { Id = fo.Id, OrgName = fo.OrgName, OrgLevel = fo.OrgLevel };
+                firstOrgList.Add(tempFo);
+            }
+            ViewData["firstOrgList"] = firstOrgList;
+
+            //装载职位类型
+            List<OccupationClass> ocList = occupationBLL.GetAllOccupationClass();
+            List<Models.OccupationClass> occClassList = new List<Models.OccupationClass>();
+            foreach (var oc in ocList)
+            {
+                Models.OccupationClass tempClass = new Models.OccupationClass { Id = oc.Id, Name = oc.Name };
+                occClassList.Add(tempClass);
+            }
+            ViewData["occClassList"] = occClassList;
+
+            //装载职称
+            List<TechnicalTitle> ttList = occupationBLL.GetAllTechnicalTitle();
+            List<Models.TechnicalTitle> tTitleList = new List<Models.TechnicalTitle>();
+            foreach (var tt in ttList)
+            {
+                Models.TechnicalTitle tempTitle = new Models.TechnicalTitle { Id = tt.Id, Name = tt.Name };
+                tTitleList.Add(tempTitle);
+            }
+            ViewData["tTitleList"] = tTitleList;
+
+
             return View();
         }
     }
